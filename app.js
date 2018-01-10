c7f5fc28649289cb1a14a39e85c26ce974efcd27
Linda O'Neill <oneill.linda21@gmail.com>
2018-01-10 13:06:53 -0600

    modified checkpoint completed

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-
   addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
 
@@ -15,8 +14,20 @@ function onReady() {
     newLi.appendChild(checkbox);
     toDoList.appendChild(newLi);
     newToDoText.value = '';
+    const deleteButton = document.createElement("button");
+    deleteButton.type = "button";
+    deleteButton.innerText = "Delete";
+    newLi.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', () => {
+      event.preventDefault();
+      toDoList.removeChild(newLi);
+    });
   });
+
 }
+
+
 
 window.onload = function() {
   alert("The window has loaded!");
